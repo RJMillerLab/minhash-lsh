e389c444d95ef5b5b85f0f749f4b3a0b12637034
BiChen Rao <erao@thor.isl.sandbox>
2016-04-16 11:22:40 -0400

    Revert "add in code for lsh.go"
    This reverts commit ca85ec410b819743637c8c8b70d4b995a476a136.

--- script/lsh.go
+++ script/lsh.go
@@ -4,11 +4,11 @@ import (
 	"experiment"
 	"flag"
 	"log"
-	"time"
 )
 
 var (
 	sigDir       string
+	domainDir    string
 	indexAttrCsv string
 	queryAttrCsv string
 	nWorker      int
@@ -21,16 +21,18 @@ var (
 func init() {
 	flag.StringVar(&sigDir, "s", "MINHASH",
 		"Directory of minhash signatures")
+	flag.StringVar(&domainDir, "d", "DOMAIN",
+		"Directory of domains")
 	flag.StringVar(&indexAttrCsv, "i", "ATTRIBUTE.csv",
 		"CSV file of index attributes with format <table_name>, <column>, <cardinality>")
 	flag.StringVar(&queryAttrCsv, "q", "ATTRIBUTE.csv",
 		"CSV file of query attributes with format <table_name>, <column>, <cardinality>")
 	flag.StringVar(&outFile, "o", "LSH.json",
 		"Output file of the lsh result")
-	flag.IntVar(&l, "l", 256, "Number of hash tables for LSH")
+	flag.IntVar(&l, "l", 128, "Number of hash tables for LSH")
 	flag.IntVar(&k, "k", 2, "Size of hash key for LSH")
 	nWorker = 200
-	ks = []int{10}
+	ks = []int{10, 20, 30, 40, 50}
 }
 
 func main() {
@@ -41,6 +43,14 @@ func main() {
 	if err != nil {
 		log.Panic(err.Error())
 	}
+	indexDomains, err := experiment.ReadAttributeDomains(indexAttrCsv, domainDir)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	queryDomains, err := experiment.ReadAttributeDomains(queryAttrCsv, domainDir)
+	if err != nil {
+		log.Panic(err.Error())
+	}
 	// Initialize result
 	result := experiment.NewAccuracyVsK(ks)
 	index, dur, err := experiment.Lsh(attributes, l, k)
@@ -48,22 +58,31 @@ func main() {
 		log.Panic(err.Error())
 	}
 	log.Printf("Indexing finished in %.4f seconds", dur)
-	for i, _ := range ks {
+	for i, k := range ks {
 		queryFunc := func(q experiment.Attribute) experiment.QueryResult {
 			r := make(chan int)
-			start := time.Now().UnixNano()
 			go func() {
 				index.Query(q.Signature, l, r)
 				close(r)
 			}()
-			end := time.Now().UnixNano()
-			diff := end - start
 			neighbours := make([]int, 0)
 			for i := range r {
 				neighbours = append(neighbours, i)
 			}
+			candidates := make([]experiment.Attribute, len(neighbours))
+			for i, id := range neighbours {
+				candidates[i] = indexDomains[id]
+			}
+			queryDomain := queryDomains[q.Id]
+			scan := experiment.NewLinearscan(candidates)
+			topk := scan.TopK(&queryDomain, k, 0.0, experiment.ExactResemblance)
+			topkNeighbours := make([]int, len(topk))
+			for i := range topk {
+				topkNeighbours[i] = topk[i].Id
+			}
 			return experiment.QueryResult{
-				Time: float64(diff),
+				Id:         q.Id,
+				Neighbours: topkNeighbours,
 			}
 		}
 		queries := experiment.AttributeSignatureChannel(queryAttrCsv, sigDir)
